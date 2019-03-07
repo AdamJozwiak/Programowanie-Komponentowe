4cbad0fac5e50bf8d5e6c61669f24f45449f846f
Michacy45 <pes.mich@gmail.com>
2019-03-07 21:21:02 +0100

    Poczatki z mavenem

--- src/main/java/com/kalk/Kalkulator.java
+++ src/main/java/com/kalk/Kalkulator.java
@@ -1,15 +1,54 @@
 package com.kalk;
-
 public class Kalkulator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public double add(double a, double b){
+        return a+b;
+    }
+
+    public int diff(int a, int b){
+        return a-b;
+    }
+
+    public double diff(double a, double b){
+        return a-b;
+    }
+
+    public int mul(int a, int b){
+        return a*b;
+    }
+
+    public double mul(double a, double b){
+        return a*b;
+    }
+
+    public int div(int a, int b){
+        // if (b!=0)
+        return a/b;
+        //else
+        //  System.out.println("Nie dzielimy przez zero!");
+    }
+
+    public double div(double a, double b){
+        //if (b!=0)
+        return a/b;
+        // else
+        //   System.out.println("Nie dzielimy przez zero!");
+    }
+
+
     public static void main(String[] args) {
 
-        int a=4;
-        int b=5;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a+b*b);
-
+        // dodawanie liczb
+        int a=0; // deklaracja zmiennej
+        int b=0;
+        Kalkulator kalkulator=new Kalkulator();
+        //System.out.println(0/0);
+        //int c=a/b;
+        // dzielenie z instrukcja warunkowa
         if (b!=0)
             System.out.println(a/b);
         else
--- src/test/java/com/kalk/KalkulatorTest.java
+++ src/test/java/com/kalk/KalkulatorTest.java
@@ -1,7 +1,50 @@
 package com.kalk;
 
-import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class KalkulatorTest {
+
+    @Test
+    public void add() {
+        Kalkulator kalkulator=new Kalkulator();
+        int c=3;
+        int a=1;
+        int b=2;
+        assertEquals(c,kalkulator.add(a,b));
+        assertEquals(Integer.MIN_VALUE,kalkulator.add(Integer.MAX_VALUE,1));
+    }
+
+    @Test
+    public void add1() {
+    }
+
+    @Test
+    public void diff() {
+    }
+
+    @Test
+    public void diff1() {
+    }
+
+    @Test
+    public void mul() {
+    }
+
+    @Test
+    public void mul1() {
+    }
+
+    @Test
+    public void div() {
+    }
+
+    @Test
+    public void div1() {
+    }
+
+    @Test
+    public void main() {
+    }
 }
