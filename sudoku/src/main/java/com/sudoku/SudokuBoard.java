2502401c6b27f86d827aef8b483617cecf6f415a
Adamj500 <adamj500@interia.pl>
2019-03-17 21:27:45 +0100

    Finito

--- sudoku/src/main/java/com/sudoku/SudokuBoard.java
+++ sudoku/src/main/java/com/sudoku/SudokuBoard.java
@@ -1,4 +1,4 @@
-package Sudoku;
+package com.sudoku;
 
 import java.util.Random;
 
---
+++ sudoku/src/test/java/com/sudoku/SudokuBoardTest.java
@@ -0,0 +1,38 @@
+package com.sudoku;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SudokuBoardTest {
+
+    @Test
+    public void fillBoard() {
+        SudokuBoard sudokuBoard = new SudokuBoard();
+        sudokuBoard.firstFill(3);
+        sudokuBoard.fillBoard(0, 0);
+        boolean wartownik = false;
+
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < 9; j++)
+            {
+                if(sudokuBoard.checkElement(i,j, sudokuBoard.getBoard()[i][j]))
+                {
+                    wartownik = true;
+                }
+                else {
+                    wartownik = false;
+                    assertFalse(wartownik);
+                }
+            }
+        }
+        assertTrue(wartownik);
+
+        SudokuBoard sudokuBoard1 = new SudokuBoard();
+        sudokuBoard1.firstFill(3);
+        sudokuBoard1.fillBoard(0,0);
+
+        assertNotEquals(sudokuBoard.getBoard(), sudokuBoard1.getBoard());
+    }
+}
