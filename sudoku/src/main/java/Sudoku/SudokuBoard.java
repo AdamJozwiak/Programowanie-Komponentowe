e7ab68eb19618590b6d73873e8c43cd82aaf7ffc
Michacy45 <pes.mich@gmail.com>
2019-03-17 20:24:43 +0100

    finito bez testow

--- sudoku/src/main/java/Sudoku/SudokuBoard.java
+++ sudoku/src/main/java/Sudoku/SudokuBoard.java
@@ -2,8 +2,9 @@ package Sudoku;
 
 import java.util.Random;
 
+
 public class SudokuBoard {
-    public int[][] board;
+    private int[][] board;
 
     public int rand() {
         Random generator = new Random();
@@ -12,112 +13,103 @@ public class SudokuBoard {
 
     public void firstFill(int n) {
         board = new int[9][9];
-        int k, l;
+        int w, k;
         for (int i = 0; i < n; i++) {
             do {
+                w = rand() - 1;
                 k = rand() - 1;
-                l = rand() - 1;
-            } while (board[k][l] != 0);
+            } while (board[w][k] != 0);
 
-            board[k][l] = rand();
+            do {
+                board[w][k] = rand();
+            } while (!checkElement(w, k, board[w][k]));
         }
     }
 
-    public boolean checkElement(int k, int l) {
+    public boolean checkElement(int w, int k, int num) {
 
+        int pom1 = (w / 3) * 3;
+        int pom2 = (k / 3) * 3;
         for (int i = 0; i < 9; i++) {
-            if (i != k && board[k][l] == board[i][l]) {
+            if (i != w && num == board[i][k]) {
                 return false;
             }
-            if (i != l && board[k][l] == board[k][i]) {
+            if (i != k && num == board[w][i]) {
+                return false;
+            }
+            if (num == board[pom1 + i / 3][pom2 + i % 3] && (pom1 + i / 3 != w || pom2 + i % 3 != k)) {
                 return false;
             }
         }
-        int pom1 = (k / 3) * 3;
-        int pom2 = (l / 3) * 3;
 
-        for (int i = 0; i < 3; i++) {
+
+        /*for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (board[k][l] == board[pom1 + i][pom2 + j] && (pom1 + i != k || pom2 + j != l)) {
+                if (board[w][k] == board[pom1 + i][pom2 + j] && (pom1 + i != w || pom2 + j != k)) {
                     return false;
                 }
             }
-        }
+        }*/
         return true;
     }
 
-    public boolean fillBoard(int k, int l) {
-        if (l == 9) {
-            return true;
+    public boolean fillBoard(int w, int k) {
+        if (w == 9) {
+            k++;
+            w = 0;
         }
+        if (k == 9) return true;
 
-        int k2 = k + 1;
-        int l2 = l;
+        if (board[w][k] != 0) return fillBoard(w + 1, k);
 
-        if (k2 == 9)
-        {
-            k2 = 0;
-            l2++;
-        }
+        for (int num = 1; num <= 9; num++) {
 
-        if (board[k][l] == 0) {
-            int pom = rand();
-            int i = 0;
-
-            do {
-                do {
-                    board[k][l] = pom;
-                    pom = pom % 9 + 1;
-                } while (!checkElement(k, l) && ++i != 9);
-
-                if (i == 9 & !checkElement(k, l)) {
-                    board[k][l] = 0;
-                    return false;
+            if (checkElement(w, k, num)) {
+                board[w][k] = num;
+                if (fillBoard(w + 1, k)) {
+                    return true;
                 }
-            } while (!fillBoard(k2, l2));
-            return true;
+
+            }
+
+            board[w][k] = 0;
         }
-        return fillBoard(k2,l2);
+
+        return false;
+
+
     }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
 
     public static void main(String args[]) {
 
         SudokuBoard sudokuBoard = new SudokuBoard();
-        sudokuBoard.board = new int [9][9];
-        //sudokuBoard.firstFill(10);
-        sudokuBoard.board[0][3] = 7;
-        sudokuBoard.board[0][5] = 4;
-        sudokuBoard.board[0][7] = 9;
-        sudokuBoard.board[0][8] = 3;
-        sudokuBoard.board[1][1] = 3;
-        sudokuBoard.board[1][2] = 9;
-        sudokuBoard.board[1][3] = 5;
-        sudokuBoard.board[1][4] = 1;
-        sudokuBoard.board[1][8] = 8;
-        sudokuBoard.board[2][3] = 8;
-        sudokuBoard.board[2][4] = 3;
-        sudokuBoard.board[2][7] = 6;
-        sudokuBoard.board[3][0] = 7;
-        sudokuBoard.board[3][1] = 8;
-        sudokuBoard.board[3][2] = 1;
-        sudokuBoard.board[4][7] = 5;
-        sudokuBoard.board[6][1] = 5;
-        sudokuBoard.board[6][2] = 3;
-        sudokuBoard.board[6][7] = 8;
-        sudokuBoard.board[7][0] = 6;
-        sudokuBoard.board[7][1] = 5;
-        sudokuBoard.board[7][5] = 5;
-        sudokuBoard.board[7][6] = 4;
-        sudokuBoard.board[7][7] = 7;
-        sudokuBoard.fillBoard(0,0);
-
-        for(int i= 0; i<9;i++)
-        {
-            for(int j= 0; j<9; j++)
-            {
-                System.out.print(sudokuBoard.board[i][j]);
+        //udokuBoard.board = new int [9][9];
+        sudokuBoard.firstFill(3);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(sudokuBoard.board[i][j] + " ");
             }
             System.out.println();
         }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        sudokuBoard.fillBoard(0, 0);
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(sudokuBoard.board[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
---
+++ sudoku/src/test/Sudoku/SudokuBoardTest.java
@@ -0,0 +1,15 @@
+package Sudoku;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SudokuBoardTest {
+
+
+    @Test
+    public void fillBoard() {
+        assertEquals(1,1);
+
+    }
+}
